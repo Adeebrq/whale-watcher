174f6ea345595acfb9a0b7b836091da10ea8c389
Adeeb Quraishi <adeebrq@gmail.com>
2025-06-27 18:34:30 +0530

    new api

--- index.ts
+++ index.ts
@@ -5,6 +5,8 @@ import cors from "cors";
 app.use(express.json())
 app.use(cors())
 
+let data: any = null
+
 app.get("/api", (req : any, res: any)=>{
     console.log("Server up!")
     res.status(200).json("server is up")
@@ -12,9 +14,13 @@ app.get("/api", (req : any, res: any)=>{
 
 app.post("/solana-webhook", (req: Request, res: Response)=>{
     const event= req.body
+    data= event
     console.log("action", JSON.stringify(event, null , 2))
     res.status(200).json({"message": event})
 
 })
 
+app.get("/webhook", (req: Request, res: Response)=>{
+    res.status(200).json(data)
+})
 app.listen(3030, ()=> console.log("server started on port 3030"))
