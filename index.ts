6d64127b19dc01e4f4993ed9756dec5ca61ac3de
Adeeb Quraishi <adeebrq@gmail.com>
2025-06-27 18:21:52 +0530

    CORS enabled

--- index.ts
+++ index.ts
@@ -1,18 +1,19 @@
 import express, { Request, Response } from "express";
 const app= express()
+import cors from "cors";
 
 app.use(express.json())
+app.use(cors())
 
 app.get("/api", (req : any, res: any)=>{
     console.log("Server up!")
     res.status(200).json("server is up")
-
 })
 
 app.post("/solana-webhook", (req: Request, res: Response)=>{
     const event= req.body
     console.log("action", JSON.stringify(event, null , 2))
-    res.status(200).send(JSON.stringify(event, null , 2))
+    res.status(200).json({"message": event})
 
 })
 
