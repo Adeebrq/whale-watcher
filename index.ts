885403ecf0588d0128b2f4fbfcf4d104131fe36a
Adeeb Quraishi <adeebrq@gmail.com>
2025-06-28 21:39:06 +0530

    debug + localstorage

--- index.ts
+++ index.ts
@@ -1,13 +1,14 @@
 import express, { Request, Response } from "express";
 const app= express()
 import cors from "cors";
+import fs from "fs"
 
 app.use(express.json())
 app.use(cors())
 
 let data: any = null
 
-app.get("/api", (req : any, res: any)=>{
+app.get("/api", (req : Request, res: Response)=>{
     console.log("Server up!")
     res.status(200).json("server is up")
 })
@@ -15,6 +16,8 @@ app.get("/api", (req : any, res: any)=>{
 app.post("/solana-webhook", (req: Request, res: Response)=>{
     const event= req.body
     data= event
+    fs.writeFileSync("webhookJson.json", JSON.stringify(event))
+
     console.log("action", JSON.stringify(event, null , 2))
     res.status(200).json({"message": event})
 
@@ -22,5 +25,13 @@ app.post("/solana-webhook", (req: Request, res: Response)=>{
 
 app.get("/webhook", (req: Request, res: Response)=>{
     res.status(200).json(data)
+    if(data === null){
+        res.status(200).json("No data is available in webhook")
+    }else{
+        // res.status(200).json(data)
+        const datafetch= fs.readFileSync("webhookJson.json", 'utf-8')
+        const datafetched= JSON.parse(datafetch)
+        res.status(200).json(datafetched)
+    }
 })
 app.listen(3030, ()=> console.log("server started on port 3030"))
